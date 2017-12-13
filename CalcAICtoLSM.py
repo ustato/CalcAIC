b797a1c06be958c2611f0660b138359868a321a4
Atsuto0519 <atsuto.fps@gmail.com>
2017-12-13 20:21:49 +0900

    Fix bag

--- CalcAICtoLSM.py
+++ CalcAICtoLSM.py
@@ -3,14 +3,16 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import LSM as LSM
+import AIC
+import LSM
 
-#データ
-data = np.loadtxt("lsmdata1_train.csv",delimiter=" ")
+
+# データ
+data = np.loadtxt("lsmdata2_train.csv",delimiter=" ")
 
 # データを表示
-#print("data")
-#print(data)
+# print("data")
+# print(data)
 
 # データをわかりやすい配列に格納(pltに使いやすくしたいから)
 data_x=[]
@@ -20,18 +22,25 @@ for i in data :
     data_y.append(i[1])
 
 # 次数Nを決定
-#N = input("N == ")
-N = 1
+N = 3
+
+print("N="+str(N))
 
 # LSMで誤差と重みベクトルcoeを計算
 error, coe = LSM.LSM(data, int(N+1))
 print("error:"+str(error))
-print("coe:"+str(coe))
+print("coe:"+str(coe[::-1]))
+
+# ここで，このモデルのAICを計算
+l = AIC.l_MAX(list(coe[::-1]),data_x,data_y)
+print("l="+str(l)+",AIC("+str(N)+")="+str(AIC.AIC(l,N+1)))
 
 # データをプロット
 plt.plot(data_x, data_y, "o")
 
 # LSMで得た近似線をプロット
-test_x = np.arange(min(data_x), max(data_x), 0.1)
+test_x = np.arange(min(data_x), max(data_x), 0.01)
 plt.plot(test_x, LSM.quation_LSM(coe, test_x))
+
+plt.grid()
 plt.show()
